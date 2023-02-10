2dbd6308073059fbfc588b301d1d89fc6539ddb2
Nicole Mantilla <nicolemf14@gmail.com>
2023-02-10 19:46:16 +0100

    elimino algunas imagenes.

--- Back/app/routes/image.routes.js
+++ Back/app/routes/image.routes.js
@@ -6,7 +6,7 @@ console.log(uploadFile)
 
 
 module.exports = function(app){
-//localhost:8081/uploadImage
+//http://localhost:8081/uploadFile
 app.post('/uploadFile', uploadFile(), (req, res)=>{
     console.log(req.file);
     res.send('ok');
