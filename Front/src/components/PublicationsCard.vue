dbe8c61461678ee750fbde1c76349860ccbd5dad
rucev <ruedaflors@gmail.com>
2023-02-13 01:18:05 +0100

    WIP New style for publication cards

--- Front/src/components/PublicationsCard.vue
+++ Front/src/components/PublicationsCard.vue
@@ -1,27 +1,45 @@
 <template>
-  <div class="max-w-sm rounded overflow-hidden shadow-lg">
-    <div class="public-img"> 
-      <img height="30" width="30" class="rounded-t-lg" :src= "`http://localhost:8081/images/${publication.id}${publication.image}`" alt="Imagen de la publicación" /> 
-    </div>
-    <div class="public-info">
-      <div class="public-title">{{ publication.title }}</div>
-      <p class="public-text">{{ publication.text }} </p>
-      <div class="public-data">
-        <div class="public-date">{{ publication.date }}</div>
-        <div class="public-author">{{ publication.author }}</div>      
-        <div class="public-likes">{{ publication.likes }} <img height="40" width="40" src="../assets/logo-eye.png"/></div>
-      </div>
-      <div class="buttons" v-if="isAuth">
-        <button class="follow-button btn-style" @click="$emit('follow', publication.author)">Seguir / Dejar de seguir</button>
-          <div class="like-buttons">
-            <button class="like-button" @click.once="$emit('like', publication)" > <img height="30" width="30" src="../assets/karmaPositive.png"/></button>
-            <button class="dislike-button" @click.once="$emit('dislike', publication)"> <img height="30" width="30" src="../assets/karmaNegative.png"/></button>
-          </div>
-        </div>
-      <div v-else class="msg"><router-link class="link" to="/login">Lógate</router-link> para poder seguir a este usuario y darle Karma
-      </div>
-    </div>
+  <div class="card card w-25 mt-2 publication">
+
+  <div class="card-header">
+    <span class="public-author">
+      {{ publication.author }}
+    </span>
+    <span class="public-date">
+      {{ publication.date }}
+    </span>
   </div>
+  <ul class="list-group list-group-flush" v-if="isAuth">
+    <li class="list-group-item">
+      <button class="follow-button btn-style" @click.once="$emit('dislike', publication)">
+        <img height="15" width="15" src="../assets/karmaNegative.png"/>
+      </button>
+      <button class="follow-button btn-style" style="margin-left: 2%; margin-right: 2%;" @click="$emit('follow', publication.author)">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-add" viewBox="0 0 16 16">
+          <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+          <path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z"/>
+        </svg>
+      </button>
+      <button class="follow-button btn-style" @click.once="$emit('like', publication)">
+        <img height="15" width="15" src="../assets/karmaPositive.png"/>
+      </button>
+    </li>
+  </ul>
+  <ul class="list-group list-group-flush" v-else>
+    <li class="list-group-item">
+      <router-link class="link" to="/login">Lógate</router-link> para poder seguir a este usuario y darle Karma
+    </li>
+  </ul>
+  <img class="public-img"  :src= "`http://localhost:8081/images/${publication.id}${publication.image}`" alt="Imagen de la publicación" />
+  <div class="card-body">
+    <h5 class="card-title">{{ publication.title }}</h5>
+    <p class="card-text">{{ publication.text }}</p>
+  </div>
+  <div class="card-footer text-muted">
+    {{ publication.likes }}
+    <img height="40" width="40" src="../assets/logo-eye.png"/>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
@@ -48,79 +66,93 @@ export default defineComponent({
 </script>
 
 <style scoped>
-    .publication {
-      width: 100%;
-      display: flex;
-      border: 2.5px color #6b7280;
-      border-radius: 10px;
-      background-color: #f3f4f6;
-      margin: 2%;
-    }
-    .public-img > img {
-      width: auto;
-      height: auto;
-      max-height: 500px;
-      max-width: 500px;
-      border-start-start-radius: 10px;
-      border-bottom-left-radius: 10px;
-    }
-    .public-info {
-      display: flexbox;
-      padding-top: 2%;  
-      font-size: 20px;
-      line-height: 84%;
-      padding-left: 1%;
-    } 
-    .public-title {
-      margin-bottom: 2%;
-      font-size: 40px;
-      font-family: 'Playfair Display', serif;
-      padding-left: 7px;
-      color: var(--color-violet700);
-    }
-    .public-text {
-      text-align: justify;
-      padding: 5%;
-      color: var(--color-violet600)
-    }
-    .public-data {
-      color: var(--color-grey200);
-      padding-right: 2%;
-    }
-    .public-data, .buttons {
-      display: flex;
-      justify-content: space-between;
-    }
-    .public-likes {
-      font-size: x-large;
-      margin-right: 10px;
-    }
-    .buttons {
-      padding: 0 1% 1% 0;
-    }
-    .follow-button {
-      border-radius: 5px;
-      font-size: small;
-    }
-    button.like-button, button.dislike-button {
-      border-color: var(--color-violet100);
-      border-radius: 50%;
-      padding: 2px;   
-      margin-right: 6px; 
-    }
-    .link{
-      color: var(--color-violet600);
-      font-weight: 800;
-      text-shadow: var(--color-green100) 1px 0 2px;
-    }
-    .link:hover, link:active {
-      color: var(--color-violet700)
-    }
-    .msg {
-      font-size: 13px;
-    }
+  .publication {
+    margin: 2%;
+  }
+  .card {
+    background-color: var(--color-violet10) !important;
+    border-color: var(--color-violet400);
+    border-style: 1px dotted;
+  }
+  .list-group-item{
+    background-color: var(--color-violet10color) !important;
+  }
+  .public-img {
+    width: auto;
+    padding: 5px; 
+    max-height: 10rem;
+    border-start-start-radius: 10px;
+    border-bottom-left-radius: 10px;
+  }
+  .public-author{
+    color: var(--color-violet700);
+    font-weight: 700;
+    margin-left: 2%;
+    margin-right: 5%;
+    text-shadow: var(--color-green100) 1px 0 2px;
+  }
+  .public-date{
+    color: var(--color-violet400);
+    font-size: 90%;
+    margin-left: 5%;
+    margin-right: 2%;
+    font-style: italic;
+  }
+  .public-info {
+    display: flexbox;
+    padding-top: 2%;  
+    font-size: 20px;
+    line-height: 84%;
+    padding-left: 1%;
+  } 
+  .card-title{
+    color: var(--color-violet700);
+    font-weight: 700;
+    text-shadow: var(--color-green100) 1px 0 2px;
+  }
+  .public-text {
+    text-align: justify;
+    padding: 5%;
+    color: var(--color-violet600)
+  }
+  .public-data {
+    color: var(--color-grey200);
+    padding-right: 2%;
+  }
+  .public-data, .buttons {
+    display: flex;
+    justify-content: space-between;
+  }
+  .public-likes {
+    font-size: x-large;
+    margin-right: 10px;
+  }
+  .buttons {
+    padding: 0 1% 1% 0;
+  }
+  .follow-button {
+    border-radius: 5px;
+    font-size: small;
 
-    @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+  }
+  button.like-button, button.dislike-button {
+    border-color: var(--color-violet100);
+    border-radius: 50%;
+    padding: 2px;   
+    margin-right: 6px; 
+  }
+  .link{
+    color: var(--color-violet600);
+    font-weight: 800;
+    text-shadow: var(--color-green100) 1px 0 2px;
+  }
+  .link:hover, link:active {
+    color: var(--color-violet700)
+  }
+  .msg {
+    font-size: 13px;
+  }
+
 
  </style>
     
